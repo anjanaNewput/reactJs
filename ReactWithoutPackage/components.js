4990522b8365bb493925bb39f349c00d30ee2f12
anjanaNewput <anjana@newput.com>
2017-06-20 14:52:45 +0530

    Load components on link's click

--- ReactWithoutPackage/components.js
+++ ReactWithoutPackage/components.js
@@ -1,40 +1,13 @@
-function ListItem(props) {
-  return <li key={props.id}>{props.name}</li>;
-}
-
 class Header extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: 'React App'
-    };
-  }
-  render() {
-    return (
-        <nav className="navbar navbar-default navbar-inverse">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <a className="navbar-brand" href="#">{this.state.name}</a>
-              </div>
-              <ul className="nav navbar-nav navbar-right">
-                <li className="active"><a href="#">Home</a></li>
-                <li><a href="#">Gallary</a></li>
-                <li><a href="#">Contact Us</a></li>
-              </ul>
-            </div>
-        </nav>
-    );
-  }
-}
-
-class NavComponent extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
+      name: 'Book Store',
       page: '',
-      listItems: [{name: 'Render List Component', id:1, page: 'List'}, {name: 'Render Form Component', id:2, page: 'Form'}, {name: 'Render Table Component', id:3, page: 'Table'}, {name: 'Render Paragraph', id:4, page: 'Para'}] 
+      bookList: [{name: 'PHP', id: 1, page: 'php'}, {name: 'JAVASCRIPT', id: 2, page: 'javascript'}, {name: 'JAVA', id: 3, page: 'java'}, {name: 'PYTHON', id: 4, page: 'python'}]
     };
   }
+  
   pageClicked(value) {
       this.setState({
           page: value
@@ -42,99 +15,118 @@ class NavComponent extends React.Component {
   }
   render() {
     return (
-        <div className="left-side-bar">
-            <ul className="list-inline">
-            {this.state.listItems.map((item) => 
-             <li key={item.id} onClick={() => this.pageClicked(item.page)}><a>{item.name}</a></li>
-            )}
-           </ul>
-           <RightComponent page={this.state.page} />
-       </div>
-    );
-  }
-}
-
-class RightComponent extends React.Component {
-  render() {
-    return (
-        <div className="right-side-bar">
-          {this.props.page == 'List'? <ListComponent /> : <FormComponent />}
-        </div>
-    );
-  }
-}
-
-class ListComponent extends React.Component {
-  constructor(){
-      super();
-      this.state = {
-         page: 'List',
-         studentList: [{name: 'Mak', id:1}, {name: 'Alice', id:2}, {name: 'Lisa', id:3}, {name: 'Jhon', id:4}, {name: 'Tim', id:5}] 
-      };
-  }
-  render() {
-    return (
-        <div className="list-comp">
-            <h3 className="h3">{this.state.page} Component</h3>
-            <ul className="student-list">
-                {this.state.studentList.map((item) => 
-                    <ListItem key={item.id}
-                 name={item.name} id={item.id}/>
-                )}
-           </ul>
-       </div>
-    );
-  }
-}
-
-class FormComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        name: '',
-        address: '',
-        choice: '',
-        page: 'Form'
-    };
-
-    this.handleFormControlChange = this.handleFormControlChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleFormControlChange(event) {
-      const target = event.target;
-      const name = target.name;
-      const value = target.value;
-      this.setState({
-        [name]: value
-      });
-  }
-  
-  handleSubmit(event) {
-    alert(this.state.name + ' from '+ this.state.address + ' opt the course ' + this.state.choice.toUpperCase());
-    event.preventDefault();
-  }
-
-  render() {
-    return (
         <div>
-            <h3 className="h3">{this.state.page} Component</h3>
-            <div className="form-container"><form>
-                <div className="form-group"><label>Name:<input className="form-control" type="text" value={this.state.name} name="name" onChange={this.handleFormControlChange} /></label></div>
-                <div className="form-group"><label>Address:<textarea className="form-control" value={this.state.address} name="address" onChange={this.handleFormControlChange} /></label></div>
-                <div className="form-group"><label>Select the course
-                    <select className="form-control" name="choice" value={this.state.choice} onChange={this.handleFormControlChange}>
-                        <option value="bsc">BSc</option>
-                        <option value="bca">BCA</option>
-                        <option value="ba">BA</option>
-                        <option value="bcom">BCom</option>
-                    </select>
-                </label></div>
-                <div className="form-group"><button className="btn btn-primary" onClick={this.handleSubmit}>Submit</button></div>
-            </form></div>
+            <nav className="navbar navbar-default" id="header">
+                <div className="container-fluid">
+                  <div className="navbar-header">
+                    <a className="navbar-brand" href="#">{this.state.name}</a>
+                  </div>
+                  <ul className="nav navbar-nav navbar-right">
+                  {this.state.bookList.map((item) => 
+                   <li key={item.id} onClick={() => this.pageClicked(item.page)}><a href="#">{item.name}</a></li>
+                  )}
+                  </ul>
+                </div>
+            </nav>
+            <div className="right-side-bar">
+              <BookComponent page={this.state.page} />
+            </div> 
         </div>
     );
   }
 }
 
-ReactDOM.render(<div><Header/><NavComponent /></div>, document.getElementById('root'));
+class FooterComponent extends React.Component {
+  render() {
+    return (
+        <div className="footer">
+            <p>&copy; 2017 Newput Inc.</p>
+            
+       </div>
+    );
+  }
+}
+
+//Book Component
+class BookComponent extends React.Component {
+    constructor(props){
+        super();
+        this.state = {
+           bookList: [],
+           phpCount: 0, 
+           javascriptCount: 0, 
+           pythonCount: 0, 
+           javaCount: 0
+        };
+        this.toggleCount = this.toggleCount.bind(this);
+    }
+    
+    getCountData(countObj) {
+        this.setState({
+            counts: countObj
+        });
+    }
+    loadData(page) {
+        $.getJSON( "./" +page + ".json").then((JsonList) => this.setState({ bookList: $.parseJSON(JSON.stringify(JsonList)) }));
+    }
+    
+    toggleCount(event) {
+        if(event.target.innerText == 'Like') {
+            this.setState({
+            [this.props.page+ 'Count']: this.state[this.props.page+'Count'] + 1
+            });
+            event.target.innerText = 'UnLike';
+        } else {
+            event.target.innerText = 'Like';
+            this.setState({
+                [this.props.page+ 'Count']: this.state[this.props.page+'Count'] - 1
+            });
+        }
+    }
+    render() {
+        this.loadData(this.props.page);
+        return (
+            <div className="container-fluid">
+               <div className="col-md-3"><LeftListComponent javaCount={this.state.javaCount} phpCount={this.state.phpCount} jsCount={this.state.javascriptCount} pythonCount={this.state.pythonCount}/></div>
+               <div className="col-md-9">
+                   <h3 className="h3">Books Component</h3>
+                   <ul className="student-list">
+                     {this.state.bookList.map((item) => 
+                       <li key={item.ID}>
+                          <div className="row">
+                              <div className="col-md-3">
+                                  <img src={item.Image} className="image" />
+                              </div>
+                              <div className="col-md-9 about-book">
+                                  <div className="title">{item.Title}</div>
+                                  <div className="desc">{item.Description}</div>
+                                  <div><button className="btn btn-primary" onClick={this.toggleCount}>{item.status}</button></div>
+                              </div>
+                          </div>
+                          </li>
+                      )}
+                   </ul>
+               </div>
+            </div>
+        );
+    }
+}
+
+class LeftListComponent extends React.Component {
+    render() {
+      return (
+          <div className="left-comp">
+              <h3 className="h3">Books</h3>
+              <ul>
+                  <li>JAVA {this.props.javaCount}</li>
+                  <li>PHP {this.props.phpCount}</li>
+                  <li>JAVASCRIPT {this.props.jsCount}</li>
+                  <li>PYTHON {this.props.pythonCount}</li>
+             </ul>
+         </div>
+      );
+    }
+}
+
+
+ReactDOM.render(<div><Header/><FooterComponent /></div>, document.getElementById('root'));
